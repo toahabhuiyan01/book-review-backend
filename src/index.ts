dbefcfb2c51129021390a16841f0d1ed976349b3
Toaha Bhuiyan <67577253+toahabhuiyan01@users.noreply.github.com>
2024-10-22 18:15:30 +0600

    feat: added aws lambda

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,16 @@
-import Express from 'express'
+import { APIGatewayProxyEvent, Context } from 'aws-lambda'
 import makeApi from './api/make-api'
 import routes from './routes'
-import Cors from 'cors'
-import { urlencoded, json } from 'body-parser'
-import dotenv from 'dotenv'
 
-dotenv.config()
-
-// here we are using express, we can use any backend framework
-
-const app = Express()
-app.use(Express.json())
-
-const api = makeApi('openapi.yaml', routes)
-
-app.use(Cors())
-app.use(json({ limit: '10mb' }))
-app.use(urlencoded({ limit: '10mb', extended: true, parameterLimit: 10000 }))
-app.use(
-	(req, res) => {
-		api.handleRequest(
-			{
-				method: req.method,
-				path: req.path,
-				body: req.body,
-				query: req.query as {[key: string]: string | string[]},
-				headers: req.headers as {[key: string]: string | string[]},
-			},
-			req,
-			res
-		)
-	}
+export const api = makeApi('openapi.yaml', routes)
+export const handler = (event: APIGatewayProxyEvent, context: Context) => api.handleRequest(
+	{
+		method: event.httpMethod,
+		path: event.path,
+		body: event.body,
+		query: event.queryStringParameters as { [_: string]: string | string[] },
+		headers: event.headers as { [_: string]: string | string[] },
+	},
+	event,
+	context,
 )
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-	console.log('Listening on 9090')
-})
-
-export default app
---
+++ src/serve-express.ts
@@ -0,0 +1,41 @@
+import Express from 'express'
+import makeApi from './api/make-api'
+import routes from './routes'
+import Cors from 'cors'
+import { urlencoded, json } from 'body-parser'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+// here we are using express, we can use any backend framework
+
+const app = Express()
+app.use(Express.json())
+
+const api = makeApi('openapi.yaml', routes)
+
+app.use(Cors())
+app.use(json({ limit: '10mb' }))
+app.use(urlencoded({ limit: '10mb', extended: true, parameterLimit: 10000 }))
+app.use(
+	(req, res) => {
+		api.handleRequest(
+			{
+				method: req.method,
+				path: req.path,
+				body: req.body,
+				query: req.query as {[key: string]: string | string[]},
+				headers: req.headers as {[key: string]: string | string[]},
+			},
+			req,
+			res
+		)
+	}
+)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+	console.log('Listening on 9090')
+})
+
+export default app
